fb9060c0b0ca71efd3467a258a1aa5d2ca5c4f88
Alec HARMON MACBOOK <alecblumenfeld@gmail.com>
2014-12-17 14:30:49 -0500

    changed from sass had to rebuild

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration
 // http://karma-runner.github.io/0.12/config/configuration-file.html
-// Generated on 2014-12-16 using
+// Generated on 2014-12-17 using
 // generator-karma 0.8.2
 
 module.exports = function(config) {
