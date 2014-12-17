c91174cd018947835c404eaaeb0c585c11e157fc
Alec HARMON MACBOOK <alecblumenfeld@gmail.com>
2014-12-17 13:49:24 -0500

    working js

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name suduWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the suduWebApp
+ */
+angular.module('suduWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name suduWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the suduWebApp
+ */
+angular.module('suduWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
